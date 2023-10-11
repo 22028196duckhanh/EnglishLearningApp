d8b360703ba8808acb96b7a8a66d52203f2459f8
manhtran22028195 <125729112+manhtran22028195@users.noreply.github.com>
2023-10-11 21:42:30 +0700

    a
    a

--- JavaFx/src/SortByWord.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class SortByWord implements Comparator<Word> {
-    @Override
-    public int compare(Word word1, Word word2) {
-        return word1.getWordTarget().compareTo(word2.getWordTarget());
-    }
-}
