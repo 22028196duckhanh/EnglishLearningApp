c12df9b97f331fcc7c9d0b92e63c0c4dff2b8075
khanhnd1812 <ngduckhanh1812@gmail.com>
2023-10-27 12:03:57 +0700

    add save history in close button

--- AppVerUI/src/main/java/Controller/MenuController.java
+++ AppVerUI/src/main/java/Controller/MenuController.java
@@ -125,6 +125,7 @@ public class MenuController implements Initializable {
     @FXML
     private void close(MouseEvent event) {
         Stage s = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        History.exportToFile();
         s.close();
     }
 
